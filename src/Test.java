03a8a2d6f251c6321e2db725b2b71c5519059286
lx <https://github.com/lixing1997/lx_repository.git>
2021-01-20 12:37:06 +0800

    修改了bug

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-       //远程仓库的代码
-        //其它同事修改
-        //本地代码
-        System.out.println("hello world 66");
+        //修改1.0的bug
+        System.out.println("试试看");
     }
 }
